d42bd7b9d41aae27919005f9cd9ba8a7ca16c45e
Bhavya Gupta <bhavyagupta2710@gmail.com>
2026-07-03 19:22:44 +0530

    Time: 552 ms (5.06%), Space: 47.6 MB (30.41%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,18 +1,25 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+
+        // Step 1: square
         for (int i = 0; i < nums.length; i++) {
             nums[i] = nums[i] * nums[i];
         }
+
+        // Step 2: bubble sort
         for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 0; j < nums.length - 1 ; j++) {
+
+            for (int j = 0; j < nums.length - 1 - i; j++) {
 
                 if (nums[j] > nums[j + 1]) {
+
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
                 }
             }
         }
+
         return nums;
     }
 }
