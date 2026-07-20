cfb1b8b794b959bae402bc7bb394b11fc7dd23ec
Bhavya Gupta <bhavyagupta2710@gmail.com>
2026-07-20 22:40:15 +0530

    Time: 17 ms (49.45%), Space: 43.6 MB (69.71%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -3,7 +3,6 @@ class Solution {
         int start = 0, maxLen = 1;
 
         for (int i = 0; i < s.length(); i++) {
-            // Odd length
             int l = i, r = i;
             while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
                 if (r - l + 1 > maxLen) {
@@ -13,8 +12,6 @@ class Solution {
                 l--;
                 r++;
             }
-
-            // Even length
             l = i;
             r = i + 1;
             while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
