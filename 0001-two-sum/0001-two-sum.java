55dc4f2b5a2efbd36c9a6d344b56a0e13bcfbd79
Bhavya Gupta <bhavyagupta2710@gmail.com>
2026-08-19 00:55:16 +0530

    Time: 41 ms (35.58%), Space: 47.3 MB (11.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
         for(int i = 0; i < nums.length; i++) {
             for(int j = i + 1; j < nums.length; j++) {
 
@@ -9,7 +8,6 @@ class Solution {
                 }
             }
         }
-
         return new int[]{};
     }
 }
