8310bf6e8da474f1ef4010e86a043a79db5639be
Bhavya Gupta <bhavyagupta2710@gmail.com>
2026-09-06 19:52:22 +0530

    Time: 0 ms (100%), Space: 44.8 MB (6.13%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,15 +1,16 @@
-class Solution {
-    public int maximumWealth(int[][] accounts) {
+class Solution{
+    public int maximumWealth(int accounts[][]){
         int maxWealth = Integer.MIN_VALUE;
-        for( int i = 0 ; i < accounts.length ; i++ ){
+        for(int i = 0; i<accounts.length;i++){
             int wealth = 0;
-            for(int j=0;j<accounts[i].length;j++){
+            for(int j =0 ; j<accounts[i].length;j++){
                 wealth = wealth + accounts[i][j];
-                }
-                 if(wealth>maxWealth){
+            }
+            if(wealth>maxWealth){
                 maxWealth=wealth;
             }
         }
-            return maxWealth;    
+                return maxWealth;
+
     }
 }
