52cecd5c1ebb5510cbba976b5a030153c1aefed3
Bhavya Gupta <bhavyagupta2710@gmail.com>
2026-09-21 01:11:59 +0530

    Time: 4 ms (20.71%), Space: 43.4 MB (25.77%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -10,9 +10,9 @@ class Solution {
         j++;
        } 
        if(i<word1.length()){
-        merged += word1.substring(i);i++;
+        merged += word1.substring(i);
        }else if(j<word2.length()){
-        merged += word2.substring(j);j++;
+        merged += word2.substring(j);
        }
        return merged;
     }
